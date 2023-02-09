c216eb0a319d35811eb37c0b9555c0ec591fd2b7
SevenRooT <seven.root.stevens@gmail.com>
2023-02-09 10:13:38 -0800

    Still working pseudocode

--- bootcampAssets/Develop/script.js
+++ bootcampAssets/Develop/script.js
@@ -2,12 +2,12 @@
 
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //button
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("#password"); //where password is diplayed
 
   passwordText.value = password;
 
@@ -15,3 +15,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// create varible as an empty container to hold generated password (done)
+// create function to generate password through collecting data from a series of prompts
+  // promt: pw length (8-128 characters; inclusive)
+  // prompt: character types (lowercase, uppercase, numeric, and/or special characters)
+    // each input is validated
+      // at least one character type must be chosen
+      // none chosen? error message and start-over/exit popup
+
