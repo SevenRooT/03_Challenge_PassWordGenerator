66db1f07e71cddbe54f9929cb968e1d5da8c5510
SevenRooT <seven.root.stevens@gmail.com>
2023-02-10 17:30:34 -0800

    establishing varibles and function bodies

--- bootcampAssets/Develop/script.js
+++ bootcampAssets/Develop/script.js
@@ -13,6 +13,42 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+
+
+  let numChoice = function () {
+    let characterNum = prompt("Please enter a Password length (number between 8-128).");
+    if (characterNum < 8 || characterNum > 128) {
+      alert("Character count MUST be between 8 and 128, inclusive");
+
+    }
+    return;
+  }
+  numChoice();
+  //(proves that typing number in prompt and clicking "okay", your choice is stored in characterNum)
+  //return characterNum;
+  let lowerCase = confirm("Lower case characters?");
+  let upperCase = confirm("Upper case characters?");
+  let numericChar = confirm("Numeric characters?");
+  let specialChar = confirm("Special characters?");
+
+  //a way to collect data sets of each character type (into an array)
+  let numSet = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  let lowerSet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  let upperSet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let specialSet = ['!', 'x22', '#', '$', '%', '&', 'x27', '()', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '/', ']', '^', '_', "'", '{', '|', '}', '~']
+
+  //concatenate these datasets
+  const allSets = numSet.concat(lowerSet, upperSet, specialSet);
+
+  //identify which of those characters were selected in <confirm>
+  //characterNum needs to be retrieved to build the proper length pw
+
+
+  //identify how many characters were chosen (build password one character at a time using for loop)
+  //randomly select that amount of characters from the data sets (math.random)
+  //use RETURN state to automatically push 'password' into view area  
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
